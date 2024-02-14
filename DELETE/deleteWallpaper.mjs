ed834cf4ab5a9cf8ac70d7c47bbbb48033a4f183
Jaime Julio <jljulioa@gmail.com>
2024-02-14 13:29:48 -0500

    delete update

--- DELETE/deleteWallpaper.mjs
+++ DELETE/deleteWallpaper.mjs
@@ -1,14 +1,21 @@
-
 import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client();
 
-
 export const deleteWallpaper = async (event, context) => {
   try {
     // Retrieve the image key from the S3 event
-    const srckey = JSON.parse(event.body || "{}")
-    
+    const srckey = event.queryStringParameters.filename;
+
+    if (!namefile) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({
+            error: "Missing required field: namefile"
+          })
+        };
+      }
+
     console.log(srckey);
 
     const deleteKey = `uploads/${srckey}`;
@@ -17,7 +24,7 @@ export const deleteWallpaper = async (event, context) => {
 
     const command = new DeleteObjectCommand({
       Bucket: process.env.BUCKET_NAME,
-      Key: thumbnailKey,
+      Key: deleteKey,
     });
 
     const response = await s3.send(command);
@@ -25,9 +32,7 @@ export const deleteWallpaper = async (event, context) => {
 
     return {
       statusCode: 200,
-      body: JSON.stringify(
-        `{The key file ${deleteKey} was deleted}`, response
-      ),
+      body: JSON.stringify(`{The key file ${deleteKey} was deleted}`, response),
     };
   } catch (error) {
     console.error("Error:", error);
